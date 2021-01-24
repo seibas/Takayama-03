919a233b33dc9fdcf6cb3fe615182f0cd2405ec3
sebastiano <36180003+seibas@users.noreply.github.com>
2021-01-24 13:37:07 +0900

    add title formatter

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import "../styles/style.css"
+
+
+
+
+
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
@@ -11,7 +16,8 @@ const ListLink = props => (
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-      <header style={{ marginBottom: `1.5rem` }}>
+     
+      <header style={{ marginBottom: `1.5rem` }}> 
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline` }}>TAKAYAMA WELCOME</h3>
         </Link>
