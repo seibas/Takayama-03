ce6caae77e78c501623dc5c46f3d16aeac987e72
sebastiano <36180003+seibas@users.noreply.github.com>
2021-02-02 01:27:50 +0900

    fixing not important

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,16 @@ import styled from "styled-components";
 
 const FooterRaper = styled.div`
 background-color: black;
-
+position:absolute;
 bottom: 0;
+padding:20px;
  min-height: 21vh;
 text-align:center;
  display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-
+left:0;
         width: 100%;
  
 
@@ -23,7 +24,7 @@ text-align:center;
 const Footer = () => {
     return (
         <FooterRaper>
-            <h3>This is the Footer</h3>
+            <p>This is the Footer</p>
         </FooterRaper>
       
     )
