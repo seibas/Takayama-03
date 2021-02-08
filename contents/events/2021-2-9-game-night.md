26d596ebaa60fbf138602869e9d810004218e55d
sebastiano <36180003+seibas@users.noreply.github.com>
2021-02-08 18:16:54 +0900

    add page event

--- contents/events/2021-2-9-game-night.md
+++ contents/events/2021-2-9-game-night.md
@@ -3,5 +3,8 @@ date: "2019-11-12"
 title: "Game Night at the Lodge: Scythe"
 ---
 
-## Game Night: Thor
+**Game Night: Thor**
+
+
 Join as at lodge to learn moore about the new game "Thor".
+Don't worry bring your friends with you .
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+function contact() {
+    return (
+        <Layout>
+            <h1>contact page</h1>
+        </Layout>
+    )
+}
+
+export default contact
