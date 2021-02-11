26b1d3abca42872915d4b87eb323aa98a7150716
sebastiano <36180003+seibas@users.noreply.github.com>
2021-02-11 11:52:16 +0900

    add image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,6 +19,7 @@ export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
      <Header />
+     
       <nav style={{ marginBottom: `1.5rem` }}> 
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline`, color: `black` }}>TAKAYAMA Logo.</h3>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import Layout from '../components/Layout';
+import styled from "styled-components"
+import Layout from '../components/Layout'
+import nature from "../images/nature.jpg"
+
+
+const NatureImage = styled.img`
+display:grid;
+margin-left:auto;
+margin-right:auto;
+max-width: 400px;
+    margin-top: 20px;
+    border-radius: 10px;
+    background-color: rgb(255, 156, 64);
+    padding: 0.21rem;
+
+
+`
+
 
 const index = () => {
   return (
+    
     <Layout>
-
-      <h1>index lore</h1>
+      
+   
+      <h1 style={{ display: `block`, fontSize: `3rem` }}>Takayama recycle</h1>
+      <span class="change">H</span>
+      <span class="change">o</span>
+      <span class="change">m</span>
+      <span class="change">e</span>  
+       <NatureImage src={"nature2.jpg"} alt="Nature" />
    </Layout>
   )
 }
