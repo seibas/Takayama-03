4f0106033672d0c5da19398a6e1d62512d795295
sebastiano <36180003+seibas@users.noreply.github.com>
2021-05-06 13:02:50 +0900

    not important

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,7 @@ const index = ({ data }) => {
     <Layout>
       
    
-      <h1 style={{ display: `block`, fontSize: `3rem` }}>Takayama recycle</h1>
-      <span class="change">H</span>
-      <span class="change">o</span>
-      <span class="change">m</span>
-      <span class="change">e</span>  
+      <h1>Takayama recycle</h1>
        <BannerImageFluid fluid={data.file.childImageSharp.fluid} alt="Nature" />
        <p className= { "natureTitle" }>Welcome to {data.site.siteMetadata.title}!✨</p>
        <div className= {indexStyles.cardContainer}>
